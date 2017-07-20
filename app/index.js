ebf8bae029b856637ad39b541a1ac44ffa40b7d4
Felix Volny <felix@volny.co>
2017-07-20 19:02:37 +0700

    propTypes and badge component

--- app/index.js
+++ app/index.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
 import './index.css'
+
+class Badge extends Component {
+  render() {
+    return (
+      <div>
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>Username: {this.props.username}</h3>
+      </div>
+    )
+  }
+}
+
+Badge.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
+}
+
+//ReactDOM.render(
+//  <Badge
+//    name="Felix Volny"
+//    username="volny"
+//    img='https://avatars3.githubusercontent.com/u/10927774?v=4&s=460'
+//  />,
+//  document.getElementById('app')
+//)
 
 class Users extends Component {
   render() {
@@ -20,6 +52,13 @@ class Users extends Component {
       </div>
     )
   }
+}
+
+Users.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    friend: PropTypes.bool.isRequired,
+  })),
 }
 
 ReactDOM.render(
