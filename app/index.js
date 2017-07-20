c124baafa91ef5c1eb6b27c3f05ebfa7d4dc7fe4
Felix Volny <felix@volny.co>
2017-07-20 18:43:43 +0700

    list of friends

--- app/index.js
+++ app/index.js
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 
-class App extends Component {
+class Users extends Component {
   render() {
+    const friends = this.props.list.filter((user) => user.friend === true)
+    const nonFriends = this.props.list.filter((user) => user.friend !== true)
     return (
       <div>
-        hi
+        <h2>Friends</h2>
+        <ul>
+          {friends.map((user) => <li key={user.name}>{user.name}</li>)}
+        </ul>
+        <h2>Non-friends</h2>
+        <ul>
+          {nonFriends.map((user) => <li key={user.name}>{user.name}</li>)}
+        </ul>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]}
+  />,
+  document.getElementById('app')
+)
