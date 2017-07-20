4b87318c7276ce0b7eb3caadc33119a6d7b0b59b
Felix Volny <felix@volny.co>
2017-07-20 17:39:33 +0700

    first component

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ webpack.config.babel.js
@@ -0,0 +1,21 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+export default {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+}
