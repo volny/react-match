e51d576780070654547a937e9dc1c14ecafa08b1
Felix Volny <felix@volny.co>
2017-07-21 15:15:46 +0700

    passing arg with .bind() instead of arrow func

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export default class Popular extends Component {
+  state = {
+    selectedLanguage: 'All'
+  }
+
+  updateLanguage = (lang) => {
+    this.setState({
+      selectedLanguage: lang
+    })
+  }
+
   render() {
     const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
       <ul className="languages">
-        {languages.map((lang) => <li key={lang}>{lang}</li>)}
+        {languages.map((lang) => {
+          return <li onClick={this.updateLanguage.bind(null, lang)} key={lang}>{lang}</li>
+        })}
       </ul>
     )
   }
