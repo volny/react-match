2f58c0977014e9a11458f923f88faf135639b276
Felix Volny <felix@volny.co>
2017-07-21 20:48:07 +0700

    webpack config to tell devServer to route all requests to '/'

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,13 +5,17 @@ export default {
   entry: './app/index.js',
   output: {
     path: resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
