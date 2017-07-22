854df1c4e54dfb1eb612624d5a10e51e9ba42fab
Felix Volny <felix@volny.co>
2017-07-22 13:02:58 +0700

    parse querystring and calculate score

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import queryString from 'query-string'
+import { battle } from '../utils/api'
 
 export default class Results extends Component {
+  componentDidMount = () => {
+    const players = queryString.parse(this.props.location.search)
+    battle([
+      players.playerOneName, players.playerTwoName
+    ]).then((results) => console.log(results))
+  }
+
   render() {
     return (
-      <div>{this.props.location.search}</div>
+      <div></div>
     )
   }
 }
--- app/utils/api.js
+++ app/utils/api.js
@@ -4,8 +4,8 @@
 
 import axios from 'axios'
 
-const id = 'YOUR_CLIENT_ID'
-const sec = 'YOUR_SECRET_ID'
+//const id = 'YOUR_CLIENT_ID'
+//const sec = 'YOUR_SECRET_ID'
 //const params = `?client_id=${id}&client_secret=${sec}`
 const params = ''
 
