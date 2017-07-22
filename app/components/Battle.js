e2a70430b8e89599ef06ea2e92c0e3dacf31bb8e
Felix Volny <felix@volny.co>
2017-07-22 08:49:49 +0700

    Battle component takes state on submit of PlayerInput

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,9 +1,86 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-export default class Battle extends Component {
+class PlayerInput extends Component {
+  state = {
+    username: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.onSubmit(this.props.id, this.state.username)
+  }
+
   render() {
     return (
-      <div>BATTLE</div>
+      <form className="column" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="header">
+          {this.props.label}
+        </label>
+        <input
+          id="username"
+          placeholder="Github Username"
+          type="text"
+          autoComplete="off"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button className="button" type="submit" disabled={!this.state.username}>
+          Submit
+        </button>
+      </form>
+      )
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+}
+
+export default class Battle extends Component {
+  state = {
+    playerOneName: '',
+    playerTwoName: '',
+    playerOneImage: null,
+    playerTwoImage: null,
+  }
+
+  handleSubmit = (id, username) => {
+    this.setState(() => {
+      const newState = {}
+      newState[id + 'Name'] = username
+      newState[id + 'Image'] = `https://github.com/${username}.png?size=200`
+      return newState
+    })
+  }
+
+  render() {
+    //{ playerOneName, playerTwoName } = this.state
+    //const [playerOneName, playerTwoName] = { this.state }
+    const [playerOneName, playerTwoName] = [this.state.playerOneName, this.state.playerTwoName]
+    console.log('1', playerOneName, playerTwoName, '2')
+    return (
+      <div>
+        <div className="row">
+          {/* shorthand if statement */}
+          {!playerOneName && <PlayerInput
+            id="playerOne"
+            label="Player One"
+            onSubmit={this.handleSubmit} />}
+          {!playerTwoName && <PlayerInput
+            id="playerTwo"
+            label="Player Two"
+            onSubmit={this.handleSubmit} />}
+        </div>
+      </div>
     )
   }
 }
