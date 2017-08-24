f4fe561934e7bb30eec7f00ad3398ed89578eff6
Felix Volny <felix@volny.co>
2017-08-24 13:15:05 +0700

    attempting various methods to use codebird

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,63 @@
 import axios from 'axios'
+import Q from 'q'
+import Codebird from 'codebird'
+import { API_KEY, API_SECRET } from './twitterSecrets'
+
+const codebird = new Codebird()
+codebird.setConsumerKey(API_KEY, API_SECRET)
+
+// NOTE
+// of all the tries below, only the callback version seems to work
+// it's such a hack anyways - forcing me to expose api secrets,
+// and dealing with a low quality and outdated mess that is codebird
+
+// callback version
+// const getTwitterUserData = (player) => {
+//   codebird.__call(
+//     'users_show',
+//     {screen_name: player},
+//     (reply, rate, error) => {
+//       const { name, followers_count, statuses_count, description, profile_image_url_https} = reply
+//       error ? console.error({error}) : console.log(
+//         {name},
+//         {followers_count},
+//         {statuses_count},
+//         {description},
+//         {profile_image_url_https}
+//         )
+//     }
+//   )
+// }
+
+// promises version - needs a promises lib like Q to work
+// const getTwitterUserData = (player) => {
+//   codebird.__call(
+//     'users_show',
+//     {screen_name: player}
+//   ).then((data) => {
+//     return data.reply
+//   }, (error) => {
+//     handleError(error)
+//   })
+// }
+// getTwitterUserData('jack').then((data) => console.log({data}))
+
+// 'Q' returns a promise that can't be resolved ...
+// const getTwitterUserData = (player) => {
+//   return Q.fcall(codebird.__call(
+//     'users_show',
+//     {screen_name: player}
+//   ))
+//   .then(data => data.reply)
+// }
+// getTwitterUserData('jack').then(data => console.log(data))
+
+// async version seems not to work
+// const getTwitterUserData = async (player) => {
+//   const data = await codebird.__call('user_show', {screen_name: player})
+//   return data.reply
+// }
+// console.log('async:', getTwitterUserData('jack'))
 
 const getProfile = async (username) => {
   const URI = `https://api.github.com/users/${encodeURIComponent(username)}`
@@ -24,6 +83,10 @@ const calculateScore = (profile, repos) => {
 
 const getUserData = async (player) => {
   const arrayOfPromises = [getProfile(player), getRepos(player)]
+
+  // just setting up my twttr
+  // getTwitterUserData(player)
+
   try {
     const data = await axios.all(arrayOfPromises)
 
---
+++ app/utils/twitterSecrets.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'CcDJ4mzWPWeRSrnLc8pMHTuhs'
+export const API_SECRET = 'b1sY9RL4xeZkgbLGuk5aJi1M2b5vTMqr7gOx9tVQ81wgwU9cSq'
