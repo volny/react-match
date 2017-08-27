b8558d28c67fe67a61647bb975bebc6ca26a531f
Felix Volny <felix@volny.co>
2017-08-27 15:30:12 +0700

    return custom object from API

--- functions/api.js
+++ functions/api.js
@@ -4,10 +4,23 @@ const secrets = require('./secrets').secrets
 
 const twitter = new Twitter(secrets)
 
+const calculateScore = (data) => {
+  return data.followers_count * 3 + data.statuses_count
+}
+
 module.exports = {
   getUserData: (player) => {
     return twitter.get('users/show', {screen_name: player})
-      .then(data => data)
+      .then(data => (
+        {
+          name: data.name,
+          score: calculateScore(data),
+          description: data.description,
+          // TODO replace the "normal" in the URL with "400x400"
+          image_url: data.profile_image_url_https
+        } 
+      ))
       .catch(error => console.log(error))
   }
 }
+
--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,12 @@ const getUserData = require('./api.js').getUserData
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.printFollowersCount = (req, res) => {
+exports.getTwitterScore = (req, res) => {
   // TODO replace this with production URL
   res.set('Access-Control-Allow-Origin', '*')
   res.set('Access-Control-Allow-Methods', 'GET')
   // TODO protect against malicious/wrong user input (e.g. ?user=ev&user=felix currently crashes to app)
   // what should happen is that anything thats not <URL>?username=<string> should return proper JSON that says query is not valid
   getUserData(req.query.username)
-    .then((data) => {
-      res.status(200).send(`${data.name} has ${data.followers_count} followers`)
-    })
+    .then(data => res.status(200).send(data))
 }
