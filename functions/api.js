6b10ffb264cd6c96e89de8392f9ac7b8009c354d
Felix Volny <felix@volny.co>
2017-08-28 19:56:06 +0700

    trim_users and separate user api request, Promise.all

--- functions/api.js
+++ functions/api.js
@@ -2,13 +2,16 @@ const Twitter = require('twitter')
 const secrets = require('./secrets')
 const twitter = new Twitter(secrets)
 
-const calculateScore = (data) => {
-  const user = data[0].user
+// TODO put this in a file ./algo.js
+const calculateScore = (user, timeline) => {
   const userScore = user.followers_count * 3 + user.statuses_count
 
-  const timelineSums = data.reduce((p, v) => ({
+  const timelineSums = timeline.reduce((p, v) => ({
     retweet_count: p.retweet_count + v.retweet_count,
     favorite_count: p.favorite_count + v.favorite_count
+    // TODO: correct for people with lots of replies/retweets, which means they have fewer tweets returned from twitter api
+    // add a count var that increments for each tweet.
+    // then in timelineScore do (...)*(30/tweets)
   }))
   const timelineScore = timelineSums.retweet_count + timelineSums.favorite_count
 
@@ -24,16 +27,26 @@ const formatImageUrl = (url) => {
 
 module.exports = {
   getUserData: (player) => {
-    return twitter.get('statuses/user_timeline', {
+    const timeline = twitter.get('statuses/user_timeline', {
       screen_name: player,
+      // no retweets
       include_rts: false,
-      count: 30
+      // no replies
+      exclude_replies: true,
+      count: 30,
+      // no user info which 10x's the size of the response
+      trim_user: true
     })
-      .then(data => {
-        const user = data[0].user
+
+    const user = twitter.get('users/show', {screen_name: player})
+
+    return Promise.all([timeline, user])
+      .then(values => {
+        const user = values[1]
+        const timeline = values[0]
         return {
           name: user.name,
-          score: calculateScore(data),
+          score: calculateScore(user, timeline),
           description: user.description,
           image_url: formatImageUrl(user.profile_image_url_https)
         }
