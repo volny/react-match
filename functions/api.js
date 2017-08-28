348f1677cfd719285082b9039c6fc0cbdd6a6cd4
Felix Volny <felix@volny.co>
2017-08-28 18:59:47 +0700

    interate over users timeline to get score

--- functions/api.js
+++ functions/api.js
@@ -3,28 +3,41 @@ const secrets = require('./secrets')
 const twitter = new Twitter(secrets)
 
 const calculateScore = (data) => {
-  const count = data.followers_count * 3 + data.statuses_count
-  return Math.floor(Math.sqrt(count))
+  const user = data[0].user
+  const userScore = user.followers_count * 3 + user.statuses_count
+
+  const timelineSums = data.reduce((p, v) => ({
+    retweet_count: p.retweet_count + v.retweet_count,
+    favorite_count: p.favorite_count + v.favorite_count
+  }))
+  const timelineScore = timelineSums.retweet_count + timelineSums.favorite_count
+
+  return Math.floor(Math.sqrt(userScore + timelineScore * 10))
+}
+
+const formatImageUrl = (url) => {
+  const toReplace = 'normal'
+  const replaceBy = '400x400'
+  const index = url.lastIndexOf(toReplace)
+  return url.substring(0, index) + replaceBy + url.substring(index + toReplace.length)
 }
 
 module.exports = {
   getUserData: (player) => {
-    const formatImageUrl = (url) => {
-      const toReplace = 'normal'
-      const replaceBy = '400x400'
-      const index = url.lastIndexOf(toReplace)
-      return url.substring(0, index) + replaceBy + url.substring(index + toReplace.length)
-    }
-
-    return twitter.get('users/show', {screen_name: player})
-      .then(data => (
-        {
-          name: data.name,
+    return twitter.get('statuses/user_timeline', {
+      screen_name: player,
+      include_rts: false,
+      count: 30
+    })
+      .then(data => {
+        const user = data[0].user
+        return {
+          name: user.name,
           score: calculateScore(data),
-          description: data.description,
-          image_url: formatImageUrl(data.profile_image_url_https)
+          description: user.description,
+          image_url: formatImageUrl(user.profile_image_url_https)
         }
-      ))
+      })
       .catch(error => console.log(error))
   }
 }
